14b895f40faefab615b78c71c0adade7ac97f42c
Matteo Casati <casati@gmail.com>
2016-11-23 14:56:07 +0100

    -

--- g2-automation.it/js/core.js
+++ g2-automation.it/js/core.js
@@ -11,6 +11,22 @@ app.init = function ()
     {
         $(this).parent("li").find(".dropdown-menu").slideToggle();
     });
+    
+    
+    $(".carousel").owlCarousel({
+        loop: true,
+        nav: false,
+        autoplay: true,
+        autoplayTimeout: 3000,
+        autoplayHoverPause: true,
+        responsive: 
+        {
+            0: { items: 1 },
+            450: { items: 2 },
+            600: { items: 3 }
+        }
+    });
+    
 };
 
 $(document).ready(app.init);
