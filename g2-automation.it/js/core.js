c8e32c241f0c2824373d65920c3f015a98f1a9bb
Matteo Casati <casati@gmail.com>
2016-11-25 15:40:10 +0100

    -

--- g2-automation.it/js/core.js
+++ g2-automation.it/js/core.js
@@ -10,22 +10,7 @@ app.init = function ()
     $(".menu .more").on("click", function ()
     {
         $(this).parent("li").find(".dropdown-menu").slideToggle();
-    });
-    
-    
-    $(".carousel").owlCarousel({
-        loop: true,
-        nav: false,
-        autoplay: true,
-        autoplayTimeout: 3000,
-        autoplayHoverPause: true,
-        responsive: 
-        {
-            0: { items: 1 },
-            450: { items: 2 },
-            600: { items: 3 }
-        }
-    });
+    });            
     
 };
 
